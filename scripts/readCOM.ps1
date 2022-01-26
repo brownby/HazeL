5af9e7b4cadf9710276d2e1edf804bd0a2ce553f
beb539 <brown@seas.harvard.edu>
2022-01-26 14:50:07 -0500

    Update Windows data download script

--- scripts/readCOM.ps1
+++ scripts/readCOM.ps1
@@ -23,11 +23,40 @@ $COM = [System.IO.Ports.SerialPort]::getportnames()
 $port = new-Object System.IO.Ports.SerialPort $COM,9600,None,8,one
 $port.open()
 
-$datetime = Get-Date -UFormat %y%m%d_%H%M%S
-
-$file = "HazeL_$datetime.txt"
 do {
-    $line = $port.ReadLine();
-    Add-Content -Path (Force-Resolve-Path $file) -Value "$line`n" -PassThru -NoNewline
+    Write-Host "Waiting for file to be sent from HazeL..."
+
+    # Read first line to create file name (this is blocking, so script will stop here until it receives a file
+    $file = $port.ReadLine()
+    Write-Host "`nFile detected: $file"
+
+    # now set a timeout for reads, so that end of file can be detected
+    $port.ReadTimeout = 5000
+
+    $outbuffer = [System.Collections.ArrayList]@()
+
+    Write-Host "Reading data from HazeL..."
+
+    $numlines = 0
+
+    while($true) {
+        try {
+            $line = $port.ReadLine()
+        }
+        # Consider "End of file" to mean that it's taken longer than 5 seconds to receive a new line
+        catch [System.TimeoutException] {
+            Write-Host ""
+            break
+        }
+        $numlines++
+
+        # Every dot means 100 lines of data have been read
+        if($numlines % 100 -eq 0) {Write-Host -NoNewline "."}
+        if($numlines % (25*100) -eq 0) {Write-Host ""}
+        $outbuffer.Add("$line`n") > $null
+    }
+    Add-Content -Path (Force-Resolve-Path $file) -Value $outbuffer -NoNewline
+    Write-Host "Done writing data to $file`n`nReady to download next file"
+    $port.ReadTimeout = -1
 }
 while ($port.IsOpen)
